c019562cc1fe6701344a227da198eff5462da0b4
user <user@yoojin-Macbook-Pro.local>
2019-08-23 17:59:28 +0900

    commit

--- bj_2667_danji_dfs.py
+++ bj_2667_danji_dfs.py
@@ -1,1 +1,15 @@
+import sys
+
 # dfs 함수를 하나 만들고
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+# map size
+n = int(sys.stdin.readline())
+# real map
+map = [list(sys.stdin.readline()) for _ in range(n)]
+
+
+
+print(n)
+print(map)
