b79c123af61a046d0c9debdee0ebd0fd2fce66d4
C-YooJin <ioi_3312@naver.com>
2020-06-28 09:27:57 +0900

    Refactor file name leet_twosum_2.py

--- leetcode/leet_twosum_2_hash.py
+++ leetcode/leet_twosum_2_hash.py
@@ -4,6 +4,6 @@ class Solution:
         i = 0
         for num in nums:
             if target - num in dic:
-                return dic[target-num], i
+                return [dic[target-num], i]
             dic[num] = i
             i += 1
