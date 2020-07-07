4fab276af41ded306435886e22b409d6aeb6f1a6
C-YooJin <ioi_3312@naver.com>
2020-07-07 22:21:29 +0900

    Add leet_reverseInteger_python.py

--- leetcode/leet_reverseInteger_python.py
+++ leetcode/leet_reverseInteger_python.py
@@ -1,16 +1,24 @@
-"""
 class Solution:
     def reverse(self, x: int) -> int:
- """
+        if x == 0:
+            return 0
 
-def temp(x):
-    if x >= (-(2**31)) and x <= ((2**31)-1):
-        x_str = str(x)
-        # 0이 여러개일 수 있음. 30, 300, 3000 모두 결과값은 3 나와야됨.
-        if x_str[:-1] == '0':
-            x_str = x_str.pop(-1)
-            print(x_str)
+        is_Negative = False
+        if x < 0:
+            is_Negative = True
 
+        x = str(abs(x))
+        i = 1
+        result = 0
+        for ch in x:
+            result += (int(ch)*i)
+            i *= 10
 
+        if is_Negative:
+            result += -1
 
-temp(300)
+        max_size = pow(2, 31)
+        if -max_size < result < max_size - 1:
+            return result
+        else:
+            return 0
