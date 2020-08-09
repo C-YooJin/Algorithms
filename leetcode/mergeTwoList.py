5962a86bb337c73441601d35a912e8035ce337ac
C-YooJin <ioi_3312@naver.com>
2020-08-09 13:27:41 +0900

    Create leetcode Merge Two Linked List

---
+++ leetcode/mergeTwoList.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None :
+            return l2
+
+        if l2 is None :
+            return l1
+
+        if (l1.val > l2.val) :
+            head = ListNode(l2.val)
+            head.next = self.mergeTwoLists(l1, l2.next)
+        else :
+            head = ListNode(l1.val)
+            head.next = self.mergeTwoLists(l1.next, l2)
+
+        return head
