eaf381a636fae78fda7752cb07bd4dc039aedec8
C-YooJin <ioi_3312@naver.com>
2020-08-30 20:25:46 +0900

    Create CodingTest/kakao_2020_1

---
+++ CodingTest/kakao_2020_1.py
@@ -0,0 +1,17 @@
+"""
+1. len(s)만큼 돌리면서 i값을 뽑아낸다. 음.. 일단 1개씩 자르는 것 부터 len(s)개 자르는 경우까지 다 해봐야 되기 때문이다.
+2. range(len(s)) for문 안에서 일어나는 작업
+    - i만큼 자르면서 list에 넣는다.
+    - list안에서 for문 돌면서 if lst[j] == lst[j+1]이면 zip_count를 하나 늘려준다. zip_count는 압축된 문자열 갯수 그거..
+    - 만약 lst[j] != lst[j+1]이면 new_s에 zip_count랑 lst[j]값을 붙여서 append(string도 append인?) 해준다
+        - 여기서 만약 zip_count가 1이면 1 없이 문자열만 붙여준다. (조건문 추가)
+    - lst[j]랑 lst[j+1]을 비교하면서 앞에서 해 준 작업을 계속 한다..
+    - 결과물을 result_list에 저장한다. 다시 2번으로 돌아가서 2개씩 잘라서 temp에 넣고 반복..
+3. result_list에 들어있는 string중 가장 짧은 갯수를 return
+"""
+
+def solution(s):
+    answer = []
+    return answer
+
+print(solution('ababcc'))
