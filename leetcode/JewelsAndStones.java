ec64a9ce86fbe413140b4106a83fa66ed162e7fc
C-YooJin <ioi_3312@naver.com>
2021-06-19 22:29:29 +0900

    [leecode/easy] Jewels and Stones (java, solve)

---
+++ leetcode/JewelsAndStones.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int count =0;
+        Set<Character> set = new HashSet();
+        for(char ch : J.toCharArray() )
+            set.add(ch);
+        for(char ch : S.toCharArray() ){
+            if( set.contains(ch) )
+                count++;
+        }
+
+        return count;
+    }
+}
