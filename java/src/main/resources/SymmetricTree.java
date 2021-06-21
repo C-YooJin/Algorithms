e3c0dda7df8751ba0d2fd0ef5671563846513b16
C-YooJin <ioi_3312@naver.com>
2021-06-22 00:16:38 +0900

    [leetcode/easy] Symmetric Tree (solve)

---
+++ java/src/main/resources/SymmetricTree.java
@@ -0,0 +1,18 @@
+package main.resources;
+
+import javax.swing.tree.TreeNode;
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return check(root.left, root.right);
+    }
+    public static boolean check(TreeNode left, TreeNode right){
+        if (left == null && right == null) return true;
+        else if (left == null || right == null) return false;
+
+        if (left.val == right.val) {
+            return check(left.right, right.left) && check(left.left, right.right);
+        }
+        else return false;
+    }
+}
