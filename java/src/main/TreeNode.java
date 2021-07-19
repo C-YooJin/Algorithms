9cdcf24539d22685a6c156151ac7947f2169d753
C-YooJin <ioi_3312@naver.com>
2021-07-20 00:55:42 +0900

    [leetcode] Binary Tree Level ... (solve)

--- java/src/main/TreeNode.java
+++ java/src/main/TreeNode.java
@@ -2,13 +2,19 @@ package main;
 
 public class TreeNode {
     int val;
-    TreeNode left;
-    TreeNode right;
+    TreeNode left;      // 왼쪽 child
+    TreeNode right;     // 오른쪽 child
+
+    // 생성자
     TreeNode() {
     }
+
+    // 파라미터가 하나인 경우
     TreeNode(int val) {
         this.val = val;
     }
+
+    // 파라미터가 두개인 경우
     TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
